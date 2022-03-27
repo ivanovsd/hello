cb383ab0417d448c05eff535127727bb75c9df15
ivanovsd <ivanovsd@gmail.com>
2022-03-27 10:31:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3030;
 
+const assetBuffer = new ArrayBuffer(8 * 1e7); // 10 миллионов * 8, 77 Мб
+const assetList = Buffer.alloc(32 * 1e7); // 10 миллионов * 32, 306 Мб
+let assetCount = 0;
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
